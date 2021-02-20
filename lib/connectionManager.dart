55c15741785ec43c21799ad7270494595d5ecb49
sherman-l <slok6@my.bcit.ca>
2021-02-20 14:48:59 +0900

    added singleton bluetooth connector

---
+++ lib/connectionManager.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_blue/flutter_blue.dart';
+
+class ConnectionManager {
+  static BluetoothDevice connectedDevice;
+  static BluetoothService connectedService;
+  static ConnectionManager instance = new ConnectionManager();
+
+  static ConnectionManager getInstance() {
+    return instance;
+  }
+
+  void setDevice(BluetoothDevice device) {
+    connectedDevice = device;
+  }
+
+  void setService(BluetoothService service) {
+    connectedService = service;
+  }
+
+  BluetoothDevice getDevice() {
+    return connectedDevice;
+  }
+
+  BluetoothService getService() {
+    return connectedService;
+  }
+}
